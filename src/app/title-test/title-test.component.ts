9faf8bb396cfbc7a8a1cbb916e944b1f9a4efef3
Thalles Conceição <thallesangelo@gmail.com>
2021-04-02 19:11:04 -0300

    Clean title-test archive

--- src/app/title-test/title-test.component.ts
+++ src/app/title-test/title-test.component.ts
@@ -1,19 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-title-test',
-  // templateUrl: './title-test.component.html',
   template: `
-    <h1>title</h1>
-    <p>Component without archive</p>
+    <h1>This is a Website</h1>
   `,
   styleUrls: ['./title-test.component.css']
 })
-export class TitleTestComponent {//implements OnInit
-
-  // constructor() { }
-
-  // ngOnInit(): void {
-  // }
-
-}
+export class TitleTestComponent {}
