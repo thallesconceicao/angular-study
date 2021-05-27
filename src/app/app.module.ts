348d4ae1a0ab58d44ae4639c7c8f26b608cf02e0
Thalles Conceição <thallesangelo@gmail.com>
2021-05-27 20:24:41 -0300

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PersonModule } from './person/person.module';
 import { TitleTestComponent } from './title-test/title-test.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TitleTestComponent } from './title-test/title-test.component';
   ],
   imports: [
     BrowserModule,
-    PersonModule
+    PersonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
